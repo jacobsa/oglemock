337dc343c5ee159a722d29ad2595c5e75bbe34ce
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-12-21 20:00:57 +1100

    Switched to a list of expectations for each method.

--- controller.go
+++ controller.go
@@ -86,12 +86,12 @@ type Controller interface {
 // NewController sets up a fresh controller, without any expectations set, and
 // configures the controller to use the supplied error reporter.
 func NewController(reporter ErrorReporter) Controller {
-	return &controllerImpl{reporter, map[string]*InternalExpectation{}}
+	return &controllerImpl{reporter, map[string][]InternalExpectation{}}
 }
 
 type controllerImpl struct {
 	reporter ErrorReporter
-	expectations map[string]*InternalExpectation
+	expectations map[string][]InternalExpectation
 }
 
 func getMapKey(o MockObject, methodName string) string {
