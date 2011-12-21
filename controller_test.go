e747a687940995558fe7e7e3e2fbccf379eb7fe4
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-12-21 16:28:24 +1100

    ControllerTest.ImplicitOneTimeActionLowerBoundNotSatisfied

--- controller_test.go
+++ controller_test.go
@@ -260,6 +260,48 @@ func (t *ControllerTest) ImplicitOneTimeActionCountNotSatisfied() {
 }
 
 func (t *ControllerTest) ImplicitOneTimeActionLowerBoundNotSatisfied() {
+	// Expectation -- add three one-time actions and a fallback.
+	partial := t.controller.ExpectCall(
+		t.mock1,
+		"StringToInt",
+		"burrito.go",
+		117)
+
+	exp := partial(HasSubstr(""))
+	exp.WillOnce(Return(0))
+	exp.WillOnce(Return(1))
+	exp.WillOnce(Return(2))
+	exp.WillRepeatedly(Return(3))
+
+	// Call twice.
+	t.controller.HandleMethodCall(
+		t.mock1,
+		"TwoIntsToString",
+		"",
+		0,
+		[]interface{}{""})
+
+	t.controller.HandleMethodCall(
+		t.mock1,
+		"TwoIntsToString",
+		"",
+		0,
+		[]interface{}{""})
+
+	// The error should not yet be reported.
+	ExpectThat(len(t.reporter.errorsReported), Equals(0))
+
+	// Finish should cause the error to be reported.
+	t.controller.Finish()
+
+	ExpectThat(len(t.reporter.errorsReported), Equals(1))
+	ExpectThat(t.reporter.errorsReported[0].fileName, Equals("burrito.go"))
+	ExpectThat(t.reporter.errorsReported[0].lineNumber, Equals(117))
+	ExpectThat(t.reporter.errorsReported[0].err, Error(HasSubstr("Unsatisfied")))
+	ExpectThat(t.reporter.errorsReported[0].err, Error(HasSubstr("StringToInt")))
+	ExpectThat(t.reporter.errorsReported[0].err, Error(HasSubstr("has substring \"\"")))
+	ExpectThat(t.reporter.errorsReported[0].err, Error(HasSubstr("called 3 times")))
+	ExpectThat(t.reporter.errorsReported[0].err, Error(HasSubstr("called 2 times")))
 }
 
 func (t *ControllerTest) ImplicitCardinalityOfOneNotSatisfied() {
@@ -280,7 +322,10 @@ func (t *ControllerTest) ExplicitCardinalitySatisfied() {
 func (t *ControllerTest) ImplicitOneTimeActionCountSatisfied() {
 }
 
-func (t *ControllerTest) ImplicitOneTimeActionLowerBoundSatisfied() {
+func (t *ControllerTest) ImplicitOneTimeActionLowerBoundJustSatisfied() {
+}
+
+func (t *ControllerTest) ImplicitOneTimeActionLowerBoundMoreThanSatisfied() {
 }
 
 func (t *ControllerTest) FallbackActionConfiguredWithZeroCalls() {
